388bd0addc8fa9eef4ff79c0ad99e2f1e09fd4de
Shabd Saran <shabd.179301181@muj.manipal.edu>
2020-04-12 19:46:01 +0530

    add CORS policy

--- src/main/java/com/test/localsdigitalservices/controllers/MainController.java
+++ src/main/java/com/test/localsdigitalservices/controllers/MainController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,6 +16,7 @@ import com.test.localsdigitalservices.utils.NewsAPIUtil;
 
 @RestController
 @RequestMapping(path = "/news/sources/top", produces = MediaType.APPLICATION_JSON_VALUE)
+@CrossOrigin
 public class MainController {
 
 	private final NewsAPIUtil newsAPIUtil;
